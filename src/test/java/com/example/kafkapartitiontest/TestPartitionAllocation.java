84f3c5492af60537f90462ece17eacd5e4db7c13
Pete Bidstrup <pete@Petes-MBP.modem>
2023-08-30 17:32:11 +1000

    fixed tests

--- src/test/java/com/example/kafkapartitiontest/TestPartitionAllocation.java
+++ src/test/java/com/example/kafkapartitiontest/TestPartitionAllocation.java
@@ -31,14 +31,13 @@ public class TestPartitionAllocation {
         Map<Integer, Integer> summary = mockKafkaTopic.getSummary();
 
 
-
         assertEquals(summary.get(0), 0);
         assertEquals(summary.get(1), 0);
         assertEquals(summary.get(2), 0);
         assertEquals(summary.get(3), 0);
         assertEquals(summary.get(4), 0);
         assertEquals(summary.get(5), 0);
-        assertThat(summary.get(6), greaterThan(200_000));
+        assertThat(summary.get(6), allOf(greaterThan(200_000), lessThan(300_000)));
         assertEquals(summary.get(7), 0);
         assertEquals(summary.get(8), 0);
         assertThat(summary.get(9), allOf(greaterThan(700_000), lessThan(800_000)));
@@ -50,7 +49,27 @@ public class TestPartitionAllocation {
     @Test
     void slightImprovement() {
 
+        MockOrderFactory mockOrderFactory = getMockOrderFactory();
 
+        Stack<Order> orders = mockOrderFactory.generateWeightedOrders(1_000_000);
+
+        MockKafkaTopic mockKafkaTopic = MockTopicGenerator
+                .mockTopic(() -> orders.pop().customerId().toString(), 10, 1_000_000);
+
+        Map<Integer, Integer> summary = mockKafkaTopic.getSummary();
+
+        //{0=70291, 1=130089, 2=80153, 3=39853, 4=69583, 5=150634, 6=180126, 7=69353, 8=70051, 9=139867}
+
+        assertThat(summary.get(0), allOf(greaterThan(70_000), lessThan(80_000)));
+        assertThat(summary.get(1), allOf(greaterThan(130_000), lessThan(140_000)));
+        assertThat(summary.get(2), allOf(greaterThan(70_000), lessThan(80_000)));
+        assertThat(summary.get(3), allOf(greaterThan(30_000), lessThan(40_000)));
+        assertThat(summary.get(4), allOf(greaterThan(60_000), lessThan(80_000)));
+        assertThat(summary.get(5), allOf(greaterThan(130_000), lessThan(180_000)));
+        assertThat(summary.get(6), allOf(greaterThan(150_000), lessThan(200_000)));
+        assertThat(summary.get(7), allOf(greaterThan(55_000), lessThan(85_000)));
+        assertThat(summary.get(8), allOf(greaterThan(70_000), lessThan(80_000)));
+        assertThat(summary.get(9), allOf(greaterThan(130_000), lessThan(140_000)));
 
 
 
@@ -61,6 +80,30 @@ public class TestPartitionAllocation {
     @Test
     void idealKey() {
 
+
+        MockOrderFactory mockOrderFactory = getMockOrderFactory();
+
+        Stack<Order> orders = mockOrderFactory.generateWeightedOrders(1_000_000);
+
+        MockKafkaTopic mockKafkaTopic = MockTopicGenerator
+                .mockTopic(() -> orders.pop().orderId().toString(), 10, 1_000_000);
+
+        Map<Integer, Integer> summary = mockKafkaTopic.getSummary();
+
+
+        System.out.println(summary);
+
+
+        assertThat(summary.get(0), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(1), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(2), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(3), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(4), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(5), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(6), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(7), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(8), allOf(greaterThan(99_000), lessThan(101_000)));
+        assertThat(summary.get(9), allOf(greaterThan(99_000), lessThan(101_000)));
 
     }
 
